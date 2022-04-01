1a8ae12586ee86df553a305ce91b372926bd96ce
FareedShaikh2692 <92871730+FareedShaikh2692@users.noreply.github.com>
2022-04-01 12:17:34 -0700

    latest commit

--- app/Http/Middleware/VerifyUser.php
+++ app/Http/Middleware/VerifyUser.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 use App\User;
-// use App\Models\User;
+
 use Illuminate\Support\Facades\Session;
 use Config;
 use Illuminate\Http\Request;
